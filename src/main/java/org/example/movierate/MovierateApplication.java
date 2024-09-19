82003832291dbc143c44a3031bb3594d8d662b64
Hubert <kordula.hubert@gmail.com>
2024-09-19 17:55:20 +0200

    Create app and connect with db

---
+++ src/main/java/org/example/movierate/MovierateApplication.java
@@ -0,0 +1,14 @@
+package org.example.movierate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovierateApplication.class, args);
+
+    }
+
+}
